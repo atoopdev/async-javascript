d873033d09aa41ace10111f30a15f51cb7217bd8
Amanda Toop <60271855+atoopdev@users.noreply.github.com>
2022-05-20 17:42:59 -0700

    use all method

--- _scripts/ajax.js
+++ _scripts/ajax.js
@@ -1,70 +1,91 @@
-// add global variable containing XHR object here
-
-let httpRequest = new XMLHttpRequest();
-
-
-// add get() function here
-function get(url){
-    return new Promise(function(resolve, reject){
-        httpRequest.open('GET', url);
-        httpRequest.onload = function(){
-            if(httpRequest.status === 200){
-            resolve(httpRequest.responseText);
-            }else{
-                // if fails grab error code
-                reject(Error(httpRequest.status));
-            }
+function get(url) {
+    return new Promise(function(resolve, reject) {
+      let httpRequest = new XMLHttpRequest();
+      httpRequest.open('GET', url);
+      httpRequest.onload = function() {
+        if (httpRequest.status === 200) {
+          // Resolve the promise with the response text
+          // success(httpRequest.responseText);
+          resolve(httpRequest.response);
+        } else {
+          // Reject the promise with the status text
+          // fail(httpRequest.status);
+          reject(Error(httpRequest.statusText));
         }
-        httpRequest.send();
-    })
-   
-}
-
-
-function tempToF(kelvin) {
-    return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
-}
-
-
-function successHandler(data) {
-    const dataObj = JSON.parse(data);
-    const weatherDiv = document.querySelector('#weather');
-    const weatherFragment = `
-        <h1>Weather</h1>
-        <h2 class="top">
-        <img
-            src="http://openweathermap.org/img/w/${dataObj.weather[0].icon}.png"
-            alt="${dataObj.weather[0].description}"
-            width="50"
-            height="50"
-        />${dataObj.name}
-        </h2>
-        <p>
-        <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | ${dataObj.weather[0].description}
-        </p>
-    `
-    weatherDiv.innerHTML = weatherFragment;
-    weatherDiv.classList.remove('hidden');
-}
-
-
-function failHandler(status){
-    console.log(status);
-    const weatherDiv = document.querySelector('#weather');
-    weatherDiv.classList.remove('hidden');
-
-}
-
-
-document.addEventListener('DOMContentLoaded', function() {
-    const apiKey = 'ce9529aa97c63e9d8872f82133abe8f9'; // ADD YOUR API KEY BETWEEN THE QUOTES
-    const url = 'https://api.openweathermap.org/data/2.5/weather?q=los+angeles&APPID=' + apiKey;
-    // add get() function call here
-    // get(url, successHandler, failHandler);
-    // console.log(get(url));
-    get(url).then(function(response){
-        successHandler(response);
+      };
+  
+      // Handle network errors
+      httpRequest.onerror = function() {
+        reject(Error('Network Error'));
+      };
+  
+      httpRequest.send();
     });
-    // successHandler(httpRequest.responseText);
+  }
+  
+  function successHandler(data) {
+    const dataObj = JSON.parse(data);
+    // const weatherDiv = document.querySelector('#weather');
+    const div = `
+          <h2 class="top">
+          <img
+              src="http://openweathermap.org/img/w/${dataObj.weather[0].icon}.png"
+              alt="${dataObj.weather[0].description}"
+              width="50"
+              height="50"
+          />${dataObj.name}
+          </h2>
+          <p>
+            <span class="tempF">${tempToF(dataObj.main.temp)}&deg;</span> | 
+            ${dataObj.weather[0].description}
+          </p>
+      `;
+    return div;
+    // weatherDiv.innerHTML = weatherFragment;
+  }
+  
+  function failHandler(status) {
+    console.log(status);
+  }
+  
+  function tempToF(kelvin) {
+    return ((kelvin - 273.15) * 1.8 + 32).toFixed(0);
+  }
+  
+  document.addEventListener('DOMContentLoaded', function() {
+    const apiKey = 'ce9529aa97c63e9d8872f82133abe8f9'; // ADD YOUR API KEY BETWEEN THE QUOTES
+    //const apiKey = '';
+    const weatherDiv = document.querySelector('#weather');
+  
+    const locations = [
+      'los+angeles,us',
+      'san+francisco,us',
+      'lone+pine,us',
+      'mariposa,us'
+    ];
+    const urls = locations.map(function(location) {
+      return `https://api.openweathermap.org/data/2.5/weather?q=${location}&APPID=${apiKey}`;
+    });
+  
     
-});
+    // all allows us to make multiple requests and wait to get all until done processing
+    Promise.all([get(urls[0]), get(urls[1]), get(urls[2]), get(urls[3])])
+      .then(function(responses) {
+        //   return array of 4 template literals
+        return responses.map(function(response) {
+            // return single literal
+          return successHandler(response);
+        });
+      })
+      .then(function(literals) {
+        weatherDiv.innerHTML = `<h1>Weather</h1>${literals.join('')}`;
+      })
+      .catch(function(status) {
+        failHandler(status);
+      })
+    //   executes regardless of success or reject
+      .finally(function() {
+        weatherDiv.classList.remove('hidden');
+      });
+  });
+  
